bee6da40cfa00989f5ac362880163a49636c3892
arturrpereira <artur.fernando@protonmail.com>
2022-05-24 22:02:58 -0300

    22:02

--- App/lista_cliente.php
+++ App/lista_cliente.php
@@ -4,22 +4,21 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <title>Lista Clientes</title>
 </head>
 <body>
 
-    <table>
+    <table class="table table-dark">
         <tr>
-            <th></th>
-            <th>Id</th>
-            <th>Nome</th>
-            <th>RG</th>
-            <th>Email</th>
-            <th>CPF</th>
-            <th>Telefone</th>
-            <th>Data Nascimento</th>
+            <th scope="col">Id</th>
+            <th scope="col">Nome</th>
+            <th scope="col">RG</th>
+            <th scope="col">Email</th>
+            <th scope="col">CPF</th>
+            <th scope="col">Telefone</th>
+            <th scope="col">Data Nascimento</th>
         </tr>
-
         <?php foreach($model->rows as $item): ?>
         <tr>
           
